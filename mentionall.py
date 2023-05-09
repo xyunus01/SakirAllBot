c811fe2b8351037b90d9f67875cc275a7c16b9cc
Yunus <109164772+xyunus01@users.noreply.github.com>
2023-05-09 13:19:18 +0300

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -11,9 +11,9 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = os.environ.get("12965344")
-api_hash = os.environ.get("ab06456c190e27560182fbce2887efd3")
-bot_token = os.environ.get("6256855944:AAEvfoexkpJ_Y0tPkwdEP2JYW1uza-SSudQ")
+api_id = os.environ.get"12965344"
+api_hash = os.environ.get"ab06456c190e27560182fbce2887efd3"
+bot_token = os.environ.get"6256855944:AAEvfoexkpJ_Y0tPkwdEP2JYW1uza-SSudQ"
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
